3ad54eeec89ca9a3fd53477c0f1518d554ef76df
Ahsen Ece <231118014@samsun.edu.tr>
2025-10-22 18:32:22 +0300

    Rails projesi: 5 model ve API endpoints

---
+++ app/controllers/api/likes_controller.rb
@@ -0,0 +1,11 @@
+class Api::LikesController < ApplicationController
+  def index
+    @likes = Like.all
+    render json: @likes
+  end
+
+  def show
+    @like = Like.find(params[:id])
+    render json: @like
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show, :create]
     resources :posts, only: [:index, :show, :create]
     resources :comments, only: [:index, :show, :create]
+    resources :categories, only: [:index, :show]
+    resources :likes, only: [:index, :show]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
